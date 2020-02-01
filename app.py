ebb8b24c4a1d79bc9cf05573d2a24ed1e33c9c7d
MohamedYasser97 <mhmd.yasser@gmail.com>
2020-02-01 19:18:13 +0200

    Prepared project for Heroku

---
+++ app.py
@@ -0,0 +1,31 @@
+import tweepy
+from flask import Flask
+from tweet_util import *
+
+
+app = Flask(__name__)
+
+
+@app.route('/elR55hgw')
+def run_job():
+    # Authenticate Twitter account
+    auth = tweepy.OAuthHandler('UooCn40p2qSs3YRNXW54kliVt',
+                               'l14v1E9vjvGT3tNamWqzxoH2ObWMcZaI7Uwpg1ZdY9k9urOD4R')
+
+    auth.set_access_token('1223203700040175616-5m0hCCA03wGaGQ6BvjlnAGcvpkk8rz',
+                          '4Wau7gTnyzWG2rQ9TrgumbIxUDEAnWK85DbJnDR52Kf4D')
+
+    api = tweepy.API(auth)
+
+    artist, song_name, lyrics = prepare_tweet_content()
+
+    tweet = get_tweet_string(artist, song_name, lyrics)
+
+    # Send the tweet
+    api.update_status(tweet)
+
+    return 'OK'
+
+
+if __name__ == '__main__':
+    app.run()
