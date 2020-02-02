da74ee8c1601ad11fa0d5764487db3d113245492
MohamedYasser97 <mhmd.yasser@gmail.com>
2020-02-02 02:37:27 +0200

    Added main entry point

--- lyricurator.py
+++ lyricurator.py
@@ -3,21 +3,22 @@ import tweepy
 from tweet_util import *
 
 
-while True:
-    # Authenticate Twitter account
-    auth = tweepy.OAuthHandler('UooCn40p2qSs3YRNXW54kliVt',
-                               'l14v1E9vjvGT3tNamWqzxoH2ObWMcZaI7Uwpg1ZdY9k9urOD4R')
+if __name__ == '__main__':
+    while True:
+        # Authenticate Twitter account
+        auth = tweepy.OAuthHandler('UooCn40p2qSs3YRNXW54kliVt',
+                                   'l14v1E9vjvGT3tNamWqzxoH2ObWMcZaI7Uwpg1ZdY9k9urOD4R')
 
-    auth.set_access_token('1223203700040175616-5m0hCCA03wGaGQ6BvjlnAGcvpkk8rz',
-                          '4Wau7gTnyzWG2rQ9TrgumbIxUDEAnWK85DbJnDR52Kf4D')
+        auth.set_access_token('1223203700040175616-5m0hCCA03wGaGQ6BvjlnAGcvpkk8rz',
+                              '4Wau7gTnyzWG2rQ9TrgumbIxUDEAnWK85DbJnDR52Kf4D')
 
-    api = tweepy.API(auth)
+        api = tweepy.API(auth)
 
-    artist, song_name, lyrics = prepare_tweet_content()
+        artist, song_name, lyrics = prepare_tweet_content()
 
-    tweet = get_tweet_string(artist, song_name, lyrics)
+        tweet = get_tweet_string(artist, song_name, lyrics)
 
-    # Send the tweet
-    api.update_status(tweet)
+        # Send the tweet
+        api.update_status(tweet)
 
-    time.sleep(1800)
+        time.sleep(1800)
