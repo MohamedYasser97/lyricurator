5305bbe6556883cacfbb23ce3d0b5175b4b8a24f
Mohamed Yasser <mhmd.yasser@gmail.com>
2022-10-03 00:02:08 +0200

    list available artists

--- console_args.py
+++ console_args.py
@@ -4,6 +4,7 @@ import argparse
 def _parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('-nt', '--notwitter', action= 'store_true', help='Prints lyrics to console instead of tweeting them')
+    parser.add_argument('-la', '--listartists', action= 'store_true', help='Gets a list of all artists')
     parser.add_argument('-a', '--artist', help='Specifies artist to get lyrics for')
 
     return parser.parse_args()
