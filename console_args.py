b5b857bf97794a61faaa1b656af4070014a02e96
Mohamed Yasser <mhmd.yasser@gmail.com>
2022-10-06 01:29:19 +0200

    add -nt and -t to a mux group

--- console_args.py
+++ console_args.py
@@ -1,17 +1,24 @@
 import argparse
 
-
 def _parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-t', '--twitter', action='store_true', help='Share lyrics on Twitter')
-    parser.add_argument('-nt', '--notwitter', action= 'store_true', help='Prints lyrics to console instead of tweeting them')
+
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-t', '--twitter', action='store_true', help='Share lyrics on Twitter')
+    group.add_argument('-nt', '--notwitter', action= 'store_true', help='Prints lyrics to console instead of tweeting them')
+
     parser.add_argument('-la', '--listartists', action= 'store_true', help='Gets a list of all artists')
     parser.add_argument('-a', '--artist', help='Specifies artist to get lyrics for')
     parser.add_argument('-fs', '--fullsong',action='store_true',help='Prints entire lyrics of song to console')
     parser.add_argument('-ls', '--listsongs', action= 'store_true', help='Prints all songs for the randomly selected artist or the one selected by the user')
     parser.add_argument('-i', '--interval', type=int, help='The interval in minutes between lyrics')
-    
-    return parser.parse_args(), parser
+
+    try:
+        return parser.parse_args(), parser
+    except SystemExit:
+        parser.usage=argparse.SUPPRESS
+        parser.print_help()
+        raise
 
 CONSOLE_ARGS = _parse_arguments()[0]
 PARSER = _parse_arguments()[1]
